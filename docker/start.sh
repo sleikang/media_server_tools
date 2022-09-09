1c94bdd4c10758da8ad26aaa53192f35d1461054
DDSRem <1448139087@qq.com>
2022-09-09 16:57:19 +0800

    fix bug & add logo

--- docker/start.sh
+++ docker/start.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 function setting {
+    touch /setting.lock
 
-touch /setting.lock
-
-ln -sf /usr/share/zoneinfo/$TZ   /etc/localtime
-echo $TZ > /etc/timezone
-
+    ln -sf /usr/share/zoneinfo/$TZ   /etc/localtime
+    echo $TZ > /etc/timezone
 }
-
 if [ ! -f /setting.lock ]; then
 	setting
 fi
+chown -R ${PUID}:${PGID} /opt
+umask ${UMASK}
 
+cat /opt/docker/EmbyChineseNameSynchronous
+echo "以PUID=${PUID}，PGID=${PGID}的身份启动程序,umask=${UMASK}"
 cd /opt
-python3 main.py
+exec su-exec ${PUID}:${PGID} python3 main.py
