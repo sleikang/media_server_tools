c57822ba1dd05cd2a2ae1c4a471878547d584928
DDSRem <1448139087@qq.com>
2022-09-09 16:31:38 +0800

    fix bug & Organize files

--- docker/start.sh
+++ docker/start.sh
@@ -7,16 +7,6 @@ touch /setting.lock
 ln -sf /usr/share/zoneinfo/$TZ   /etc/localtime
 echo $TZ > /etc/timezone
 
-mkdir -p /opt/config
-
-if [ ! -f /opt/config/config.yaml ]; then
-    cp /home/config.yaml /opt/config/config.yaml
-fi
-
-if [ ! -f /opt/config/data.db ]; then
-    cp /home/data.db /opt/config/data.db
-fi
-
 }
 
 if [ ! -f /setting.lock ]; then
