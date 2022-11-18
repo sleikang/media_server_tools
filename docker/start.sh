5ab05cb7be62cc0f3801cd6561ecb2f22466a9e5
DDSRem <1448139087@qq.com>
2022-11-18 21:31:00 +0800

    fix bug

--- docker/start.sh
+++ docker/start.sh
@@ -29,7 +29,7 @@ if [ "$ECNS_AUTO_UPDATE" = "true" ]; then
     echo "更新程序..."
     git remote set-url origin ${REPO_URL} &>/dev/null
     echo "config/" > .gitignore
-    git clean -dffx
+    git clean -ffx
     git reset --hard HEAD
     git pull
     if [ $? -eq 0 ]; then
