d1feff20184fbcfa0d48a35c6e778b27c1ba69ff
sleikang <735609378@qq.com>
2022-11-19 09:45:41 +0800

    更新 test.py

--- test.py
+++ test.py
@@ -6,7 +6,10 @@ from system.log import log
 
 if __name__ == '__main__':
     try:
-        path = os.path.join(os.getcwd(), 'config', 'config.yaml')
+        if 'EMBYTOOLS_CONFIG' not in os.environ:
+            os.environ['EMBYTOOLS_CONFIG'] = os.path.join(os.getcwd(), 'config')
+
+        path = os.path.join(os.environ['EMBYTOOLS_CONFIG'], 'config.yaml')
         configinfo = config(path=path)
         mediaclient = media(configinfo=configinfo)
         while True:
